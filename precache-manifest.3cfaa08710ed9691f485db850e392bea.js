799dceb4317e6a6c7aaefdd8b56bc43a9ea68643
shinyiho <gbb2237@gmail.com>
2020-12-09 14:41:15 -0500

    Updates

---
+++ precache-manifest.3cfaa08710ed9691f485db850e392bea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488047664f3a0c2919d64f18b8ce7fca",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a641cc655d99602f93fa",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c419b2e643065fbbc76a",
+    "url": "/covid-19-tracker/static/css/main.c15710e4.chunk.css"
+  },
+  {
+    "revision": "a641cc655d99602f93fa",
+    "url": "/covid-19-tracker/static/js/2.85d1ea64.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid-19-tracker/static/js/2.85d1ea64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c419b2e643065fbbc76a",
+    "url": "/covid-19-tracker/static/js/main.0222a55a.chunk.js"
+  },
+  {
+    "revision": "5bdbd92167a555c8ec71",
+    "url": "/covid-19-tracker/static/js/runtime-main.81159ed7.js"
+  }
+]);
